40e2d7e3d62b087b0bee62a2d2d6856615041c84
Igor <4430704@gmail.com>
2024-12-08 12:12:22 +0200

    add people table

---
+++ src/components/Table.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import TableRow from './TableRow';
+import { Person, PersonFields } from '../types';
+
+type Props = {
+  people: Person[];
+};
+
+const Table: FC<Props> = ({ people }) => {
+  console.log(people);
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          {Object.values(PersonFields).map(field => (
+            <th key={field}>{field}</th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <TableRow key={person.slug} person={person} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
